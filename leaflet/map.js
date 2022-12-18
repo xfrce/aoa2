33438078a30aeba7209aa842049251e320f05b19
xfrce <xfreelance@gmail.com>
2022-12-18 18:07:06 -0500

    first commit

---
+++ leaflet/map.js
@@ -0,0 +1,11 @@
+console.log('qwry yyyy');
+/*
+let mapOptions = {
+    center: [17.385044, 78.486671],
+    zoom: 10
+}
+let map = new L.map("map", mapOptions);
+let layer = new L.TileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
+       
+map.addLayer(layer);
+*/
